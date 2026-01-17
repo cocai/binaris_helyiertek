6403c2ca9031a16a88b898dcb684c58e52d8944a
cocai <pappbali0723@gmail.com>
2026-01-17 12:25:05 +0100

    First upload

---
+++ binaris_helyiertek.py
@@ -0,0 +1,10 @@
+bekeres = input("Adj meg egy bin sorozatot: ")
+helyi_ertek = 0
+
+for i in range(len(bekeres)):
+    helyi_ertek += 2**len(bekeres[i:-1]) * int(bekeres[i])
+
+    megoldas = 2**len(bekeres[i:-1])
+    print(megoldas, "*", bekeres[i])
+
+print(f"Az eredmény: {helyi_ertek}")
